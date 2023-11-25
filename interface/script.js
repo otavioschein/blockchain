16054ec8f68b48db2995dc02b869eeab68cbe355
otaviopohren <otavio.pohren@ntconsult.com.br>
2023-11-25 00:45:45 -0300

    some changes

--- interface/script.js
+++ interface/script.js
@@ -5,6 +5,7 @@ function submitForm() {
     const mediaG2 = document.getElementById('mediaG2').value;
     const mediaFinal = document.getElementById('mediaFinal').value;
     const disciplina = document.getElementById('disciplina').value;
+    const key = document.getElementById('chave').value;
 
     const data = {
         matricula,
@@ -16,7 +17,7 @@ function submitForm() {
     };
 
     // Use fetch API to send data to your server
-    fetch('http://localhost:3001/mine', {
+    fetch(`http://localhost:3001/mine/${key}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -34,39 +35,20 @@ function submitForm() {
     });
 }
 
-function consultarNotas() {
-    const consultaMatricula = document.getElementById('consultaMatricula').value;
+function getGrade() {
+    const hash = document.getElementById('hash').value;
+    const key = document.getElementById('chave_privada').value;
 
-    fetch(`http://localhost:3001/nota?matricula=${consultaMatricula}`)
+    fetch(`http://localhost:3001/nota/${hash}/${key}`)
         .then(response => response.json())
         .then(data => {
             const resultContainer = document.getElementById('resultContainer');
             resultContainer.innerHTML = '';
 
-            if (Array.isArray(data)) {
-                // Se data for um array
-                if (data.length === 0) {
-                    resultContainer.innerHTML = '<p>Nenhuma nota encontrada.</p>';
-                } else {
-                    data.forEach(nota => {
-                        const notaInfo = document.createElement('p');
-                        notaInfo.textContent = `Disciplina: ${nota.disciplina}, Média Final: ${nota.mediafinal}`;
-                        resultContainer.appendChild(notaInfo);
-                    });
-                }
-            } else if (typeof data === 'object') {
-                // Se data for um objeto único, criamos uma lista temporária
-                const notaList = [data];
-                notaList.forEach(nota => {
-                    const notaInfo = document.createElement('p');
-                    notaInfo.textContent = `Disciplina: ${nota.disciplina}, Média Final: ${nota.mediafinal}`;
-                    resultContainer.appendChild(notaInfo);
-                });
-            } else {
-                // Se a resposta não for um array ou objeto
-                console.error('Formato de resposta inesperado:', data);
-                alert('Erro ao consultar as notas.');
-            }
+            const notaInfo = document.createElement('p');
+            console.log('Nota:', data)
+            notaInfo.textContent =`Aluno: ${data.aluno}, Disciplina: ${data.disciplina}, Média G1: ${data.mediaG1}, Média G2: ${data.mediaG2}, Média Final: ${data.mediaFinal}`;
+            resultContainer.appendChild(notaInfo);
         })
         .catch(error => {
             console.error('Error:', error);
@@ -74,45 +56,25 @@ function consultarNotas() {
         });
 }
 
-function consultarNotas() {
+function getBlockchain() {
+    fetch(`http://localhost:3001/blocks`)
+        .then(response => response.json())
+        .then(data => {
+            const resultContainer = document.getElementById('resultContainerBlockchain');
+            resultContainer.innerHTML = '';
 
-    const resultados = [
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-        { matricula: '123', aluno: 'João', disciplina: 'Matemática', mediaG1: 8.5, mediaG2: 9.0, mediaFinal: 8.8 },
-
-        // Adicione mais resultados conforme necessário
-    ];
-
-    const resultContainer = document.getElementById('resultContainer');
-
-    // Limpa o conteúdo anterior
-    resultContainer.innerHTML = '';
-
-    // Cria dinamicamente o componente de resultado para cada resultado retornado
-    resultados.forEach(resultado => {
-        const resultComponent = document.createElement('div');
-        resultComponent.classList.add('result-component');
-
-        const resultDetails = document.createElement('div');
-        resultDetails.classList.add('result-details');
-
-        // Coluna 1: matricula, aluno e disciplina
-        const column1 = document.createElement('div');
-        column1.innerHTML = `<p>Matrícula: ${resultado.matricula}</p><p>Aluno: ${resultado.aluno}</p><p>Disciplina: ${resultado.disciplina}</p>`;
-        resultDetails.appendChild(column1);
-
-        // Coluna 2: mediaG1, mediaG2 e mediaFinal
-        const column2 = document.createElement('div');
-        column2.innerHTML = `<p>Média G1: ${resultado.mediaG1}</p><p>Média G2: ${resultado.mediaG2}</p><p>Média Final: ${resultado.mediaFinal}</p>`;
-        resultDetails.appendChild(column2);
-
-        resultComponent.appendChild(resultDetails);
-        resultContainer.appendChild(resultComponent);
-    });
+            for (const block of data) {
+                const blockInfo = document.createElement('p');
+                blockInfo.textContent = JSON.stringify(block);
+                resultContainer.appendChild(blockInfo);
+            }
+            // const notaInfo = document.createElement('p');
+            // console.log('Nota:', data.toString())
+            // notaInfo.textContent = data.toString();
+            // resultContainer.appendChild(notaInfo);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Erro ao consultar as notas.');
+        });
 }
-
